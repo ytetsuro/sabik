9a9b57c2e985e3d43bbe535a700892a1883245a2
Tetsuro Yoshikawa <phper.0o0@gmail.com>
2020-11-24 00:47:54 +0900

    feat: add getName method.

--- src/Language/PHP/ASTNode.ts
+++ src/Language/PHP/ASTNode.ts
@@ -2,19 +2,34 @@ import * as PHPParser from 'php-parser';
 import { ASTNode as ASTNodeInterface } from '../../Analyzer/Adapter/ASTNode';
 
 type Node = PHPParser.Program|PHPParser.Node|PHPParser.Block;
+type IdentifierNode = PHPParser.Node & {name: string};
+type StructureNode = PHPParser.Node & {name: IdentifierNode};
+type MethodNode = StructureNode & {arguments: StructureNode[]};
+type NameExtractableNode = PHPParser.Node & {
+  name?: string|IdentifierNode,
+  value?: string
+};
+type EntryNode = PHPParser.Node & {key?: NameExtractableNode};
+type AssignNode = PHPParser.Node & {left: NameExtractableNode};
 
 enum Kind {
   CLASS = 'class',
   TRAIT = 'trait',
   METHOD = 'method',
   FUNCTION = 'function',
+  CLOSURE = 'closure',
   ALLOW_FUNCTION = 'arrowfunc',
+  ENTRY = 'entry',
+  ASSIGN = 'assign',
+  ARRAY = 'array',
+  PROPERTY_LOOKUP = 'propertylookup',
+  STATIC_LOOKUP = 'staticlookup',
 };
 
 export class ASTNode implements ASTNodeInterface {
   constructor(
     public readonly node: Node,
-    private readonly parentNode?: Node
+    private readonly parentNode?: ASTNode
   ) {}
 
   isClass() {
@@ -26,8 +41,64 @@ export class ASTNode implements ASTNodeInterface {
   }
 
   getName() {
-    return '';
+    let result = '';
+    if (this.isClass()) {
+      result = this.extractNameString((<StructureNode>this.node).name) ?? 'Anynomus Class';
+    } else if (this.isMethod()) {
+      const methodName = this.extractNameString((<StructureNode>this.node).name) ?? 'Anynomus Class';
+      const params = (<MethodNode>this.node).arguments
+        .map(row => this.extractNameString(row))
+        .filter(row => row)
+        .map(row => `$${row}`);
+
+      result = `${methodName}(${params.join(', ')})`
+    } else if (this.isFunction()) {
+      return this.getFunctionName(this);
+    }
+
+    return result;
   }
+
+  private getFunctionName(node: ASTNode, args: string[] = []): string {
+    if (node.node.kind === Kind.FUNCTION) {
+      return this.extractNameString(node.node) ?? 'Anonymous Function';
+    }
+
+    const currentNode = node?.parentNode?.node;
+    switch (currentNode?.kind) {
+      case Kind.ARRAY:
+        break;
+      case Kind.ENTRY:
+        args.push(this.extractNameString((<EntryNode>currentNode)?.key) ?? '[]');
+        break;
+      case Kind.ASSIGN:
+        args.push(`$${this.extractNameString((<AssignNode>currentNode)?.left) ?? ''}`);
+
+        return args.reverse().join('.');
+      default:
+        return 'Anonymous Function';
+    }
+
+    return this.getFunctionName(<ASTNode>node.parentNode, args);
+  }
+
+  private extractNameString(node?: NameExtractableNode) {
+     const name = node?.name ?? node?.value;
+     const result = typeof name === 'string' ? name : name?.name ?? null ; 
+
+     if (result !== null) {
+       return result;
+     } else if ([Kind.PROPERTY_LOOKUP, Kind.STATIC_LOOKUP].includes(<Kind>node?.kind)) {
+       return node?.loc.source
+        .split('=', 2)[0]
+        .replace(/[\n|\r|\s|\t]+/g, '')
+        .replace(/(\->|::)/g, '.')
+        .replace(/^\$/, '');
+     }
+
+     return null;
+  }
+
 
   isFunction() {
     return [Kind.FUNCTION, Kind.ALLOW_FUNCTION, Kind.CLOSURE].includes(<Kind>this.node.kind);
--- src/Language/PHP/__tests__/ASTNode.test.ts
+++ src/Language/PHP/__tests__/ASTNode.test.ts
@@ -11,7 +11,8 @@ describe('ASTNode', () => {
       extractDoc: true,
     },
     ast: {
-      withPositions: true
+      withPositions: true,
+      withSource: true,
     }
   });
   describe('.isClass()', () => {
@@ -120,95 +121,88 @@ describe('ASTNode', () => {
       expect(actual[1]).toBeInstanceOf(ASTNode);
     });
   });
-/*
+
   describe('.getName()', () => {
     it('should get class name.', () => {
-      const sourceFile = ts.createSourceFile(
-        'dummy.ts',
-        'class A {constructor() {} methodA() {}}',
-        ts.ScriptTarget.ES2016,
-        true
-      );
+      const sourceFile = engine.parseEval(`
+      class A {
+        function __construct() {}
+        function methodA() {}
+      }`);
 
-      const classStructure = new ASTNode(sourceFile.statements[0], sourceFile);
+      const classStructure = new ASTNode(sourceFile.children[0]);
 
       expect(classStructure.getName()).toBe('A');
     });
 
     it('should get method name.', () => {
-      const sourceFile = ts.createSourceFile(
-        'dummy.ts',
-        'class A {constructor(arg:number) {} methodA(arg:string) {}}',
-        ts.ScriptTarget.ES2016,
-        true
-      );
+      const sourceFile = engine.parseEval(`
+      class A {
+        function __construct(string $arg) {}
+        function methodA(array $args) {}
+      }`);
 
       const constructorStructure = new ASTNode(
-        (<ts.ClassDeclaration>sourceFile.statements[0]).members[0],
-        sourceFile
+        (<ClassStatement>sourceFile.children[0]).body[0]
       );
       const methodStructure = new ASTNode(
-        (<ts.ClassDeclaration>sourceFile.statements[0]).members[1],
-        sourceFile
+        (<ClassStatement>sourceFile.children[0]).body[1]
       );
 
-      expect(constructorStructure.getName()).toBe('constructor(arg)');
-      expect(methodStructure.getName()).toBe('methodA(arg)');
+      expect(constructorStructure.getName()).toBe('__construct($arg)');
+      expect(methodStructure.getName()).toBe('methodA($args)');
     });
 
     it('should get function name.', () => {
-      const sourceFile = ts.createSourceFile(
-        'dummy.ts',
-        'function a() {}',
-        ts.ScriptTarget.ES2016,
-        true
-      );
+      const sourceFile = engine.parseEval(`
+        function a() {}
+      `);
 
       const functionStructure = new ASTNode(
-        sourceFile.statements[0],
-        sourceFile
+        sourceFile.children[0],
       );
 
       expect(functionStructure.getName()).toBe('a');
     });
 
     it('should get variable name when anonymus function in variable.', () => {
-      const sourceFile = ts.createSourceFile(
-        'dummy.ts',
-        'const a = () => {};',
-        ts.ScriptTarget.ES2016,
-        true
-      );
+      const sourceFile = engine.parseEval(`
+        $a = function () {};
+      `);
 
-      const functionStructure = new ASTNode(
-        (<ts.VariableStatement>(
-          sourceFile.statements[0]
-        )).declarationList.declarations[0].initializer!,
-        sourceFile
-      );
+      const functionStructure = (new ASTNode(sourceFile)).getChilds()[0].getChilds()[0].getChilds()[1];
 
-      expect(functionStructure.getName()).toBe('a');
+      expect(functionStructure.getName()).toBe('$a');
     });
 
-    it('should get chaining variable name when anonymus function in object key.', () => {
-      const sourceFile = ts.createSourceFile(
-        'dummy.ts',
-        'const a = {b: () => {}};',
-        ts.ScriptTarget.ES2016,
-        true
-      );
+    it('should get chaining variable name when anonymus function in array key.', () => {
+      const sourceFile = engine.parseEval(`
+        $a = ['b' => fn() => 1];
+      `);
 
-      const functionStructure = new ASTNode(
-        (<ts.PropertyAssignment>(
-          (<ts.ObjectLiteralExpression>(
-            (<ts.VariableStatement>sourceFile.statements[0]).declarationList
-              .declarations[0].initializer!
-          )).properties[0]
-        )).initializer,
-        sourceFile
-      );
+      const functionStructure = (new ASTNode(sourceFile))
+        .getChilds()[0]
+        .getChilds()[0]
+        .getChilds()[1]
+        .getChilds()[0]
+        .getChilds()[1];
 
-      expect(functionStructure.getName()).toBe('a.b()');
+      expect(functionStructure.getName()).toBe('$a.b');
     });
-  });*/
+
+    it('should get chaining property name when anonymus function', () => {
+      const sourceFile = engine.parseEval(`
+        $a->c
+          ->d
+          ::$c = fn() => 1;
+      `);
+
+      const functionStructure = (new ASTNode(sourceFile))
+        .getChilds()[0]
+        .getChilds()[0]
+        .getChilds()[1];
+
+      expect(functionStructure.getName()).toBe('$a.c.d.$c');
+    });
+  });
 });
