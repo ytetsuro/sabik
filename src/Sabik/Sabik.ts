2b39099310932411e580bc043262b5c4625344e5
Tetsuro Yoshikawa <phper.0o0@gmail.com>
2021-05-17 23:02:56 +0900

    refactor: fix analyzer process for refactored modules.

--- src/Sabik/Sabik.ts
+++ src/Sabik/Sabik.ts
@@ -2,61 +2,20 @@ import { FileFinder } from './FileFinder/FileFinder';
 import { Reporter } from './Reporter';
 import { inject, injectable } from 'inversify';
 import { Types } from '../types/Types';
-import { Language } from './Language/Language';
-import { Analyzer } from '../Analyzer/Analyzer';
-import { Calculator as CognitiveComplexity } from '../Calculator/CognitiveComplexity/Calculator';
-import { Calculator as Halstead } from '../Calculator/Halstead/Calculator';
-import { Calculator as LineOfCode } from '../Calculator/LineOfCode/Calculator';
-import { ASTNodeExtractor } from '../Analyzer/ASTNodeExtractor';
-import { Calculator as Maintainability } from '../Calculator/Maintainability/Calculator';
-import { MethodAnalyzer } from '../Analyzer/ASTAnalyzer/MethodAnalyzer';
-import { FileAnalyzer } from '../Analyzer/ASTAnalyzer/FileAnalyzer';
-import { ExtractMethodMetricsAnalyzer } from '../Analyzer/MetricsAnalyzer/ExtractMethodMetricsAnalyzer';
+import { Analyzer } from './Analyzer/Analyzer';
 
 @injectable()
 export class Sabik {
   constructor(
-    private language: Language,
+    private analyzer: Analyzer,
     private fileFinder: FileFinder,
-    @inject(Types.reporter) private presenter: Reporter
+    @inject(Types.reporter) private presenter: Reporter,
   ) {}
 
   exec(findPath: string) {
-    const paths = this.fileFinder.find(findPath);
-    const extractor = new ASTNodeExtractor();
-    const analyzer = new Analyzer(
-      this.language.createASTNodeGenerator(),
-      [
-        new MethodAnalyzer(
-          extractor,
-          this.language.createCountableNodeConverter('complexityConverter'),
-          new CognitiveComplexity()
-        ),
-        new MethodAnalyzer(
-          extractor,
-          this.language.createCountableNodeConverter('halsteadConverter'),
-          new Halstead()
-        ),
-        new MethodAnalyzer(
-          extractor,
-          this.language.createCountableNodeConverter('lineOfCodeConverter'),
-          new LineOfCode()
-        ),
-        new FileAnalyzer(
-          this.language.createCountableNodeConverter('lineOfCodeConverter'),
-          new LineOfCode()
-        ),
-      ],
-      [new ExtractMethodMetricsAnalyzer(new Maintainability())]
-    );
+    const files = this.fileFinder.find(findPath);
+    const fileMetricsList = this.analyzer.analyze(files);
 
-    const targetPaths = paths.filter((path) =>
-      this.language.isSupport(path.extension)
-    );
-
-    const fileMetricsList = analyzer.analyze(targetPaths);
-
-    extractor.clear();
     this.presenter.output(fileMetricsList);
   }
 }
--- src/Sabik/__tests__/Sabik.test.ts
+++ src/Sabik/__tests__/Sabik.test.ts
@@ -1,9 +1,7 @@
-import { ASTNode } from '../../TestHelpers/ASTNode';
-import { ComplexityCountableNode } from '../../TestHelpers/ComplexityCountableNode';
-import { HalsteadCountableNode } from '../../TestHelpers/HalsteadCountableNode';
-import { LineOfCodeCountableNode } from '../../TestHelpers/LineOfCodeCountableNode';
+import { Metrics } from '../../Analyzer/Metrics/Metrics';
+import { getDouble } from '../../TestHelpers/getDouble';
+import { Analyzer } from '../Analyzer/Analyzer';
 import { File } from '../FileFinder/File';
-import { Language } from '../Language/Language';
 import { Reporter } from '../Reporter';
 import { Sabik } from '../Sabik';
 
@@ -13,46 +11,12 @@ describe('Sabik', () => {
     let reporter: jest.Mocked<Reporter>;
     beforeEach(() => {
       reporter = { output: jest.fn((_) => Promise.resolve()) };
-      const language = new Language([
-        {
-          extensions: ['.txt'],
-          complexityConverter: {
-            convert: (_) =>
-              new ComplexityCountableNode({
-                DSL: 'IN',
-                children: [{ DSL: 'I' }, { DSL: '' }],
-              }),
-          },
-          halsteadConverter: {
-            convert: (_) =>
-              new HalsteadCountableNode({
-                DSL: 'T',
-                text: '+',
-                children: [
-                  { DSL: 'N', text: '1' },
-                  { DSL: '', text: 'dummy' },
-                ],
-              }),
-          },
-          lineOfCodeConverter: {
-            convert: (_) =>
-              new LineOfCodeCountableNode(`1
-                        2
-                        // 3
-                        4`),
-          },
-          astGenerator: {
-            generate: () =>
-              new ASTNode(':root:0:0', {
-                'F:dummyFauxMethod1:1:9': {},
-              }),
-          },
-          astNodeConstructor: ASTNode,
-        },
-      ]);
+      const analyzer = <Analyzer>getDouble(Analyzer, {
+        analyze: (fileList: File[]) => fileList.map(file => new Metrics(file, [], [])), 
+      });
 
       sabik = new Sabik(
-        language,
+        analyzer,
         <any>{
           find: () => [
             new File('/tmp/foo.txt', 'foo.txt'),
@@ -63,13 +27,13 @@ describe('Sabik', () => {
       );
     });
 
-    it('should analyze only analyzeable source.', async () => {
+    it('should analyze source code.', async () => {
       await sabik.exec('./');
 
       expect(reporter.output.mock.calls[0][0].length).toBe(2);
       expect(
         reporter.output.mock.calls[0][0].map(({ file }) => file.relativePath)
-      ).toStrictEqual(['foo.txt', 'foo.txt']);
+      ).toStrictEqual(['foo.txt', 'bar.md']);
     });
   });
 });
