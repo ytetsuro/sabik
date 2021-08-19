63323402f22623a8ddce932eab4e097767a1ff23
Tetsuro Yoshikawa <phper.0o0@gmail.com>
2021-08-19 23:54:42 +0900

    fix: Change to common use case report logic.

--- src/Reporter/Reporter.ts
+++ src/Reporter/Reporter.ts
@@ -2,22 +2,14 @@ import { inject, injectable, multiInject } from 'inversify';
 import { Types } from '../types/Types';
 import { FileBuilder } from './FileBuilder';
 import { Metrics } from '../Analyzer/Metrics/Metrics';
-import { statSync } from 'fs';
-import { existsSync } from 'fs';
 
 @injectable()
 export class Reporter {
   constructor(
     @multiInject(Types.outputFileBuilder)
     private readonly builders: FileBuilder[],
-    @inject(Types.outputPath) private readonly outputPath: string
+    @inject(Types.outputPath) private readonly outputPath?: string
   ) { 
-    const exists = existsSync(outputPath);
-    const fileStat = statSync(outputPath);
-
-    if (exists && !fileStat.isDirectory()) {
-      throw new Error(`The file path is specified.Please specify the directory path.`);
-    }
   }
 
   async output(metrics: Metrics[]) {
