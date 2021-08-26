4507394486411c86be3fa6af63f8a95ebb7aa45d
Tetsuro Yoshikawa <phper.0o0@gmail.com>
2021-08-27 01:55:46 +0900

    refactor: due to there is not much maintenance for oclif.

--- src/index.ts
+++ src/index.ts
@@ -2,52 +2,29 @@ import 'reflect-metadata';
 import 'core-js/features/array';
 import { dirname, resolve } from 'path';
 import * as fs from 'fs';
-import { Command, flags } from '@oclif/command';
 import { Sabik as Main } from './Sabik/Sabik';
 import { container } from './Sabik/DIContainer';
 import { Types } from './types/Types';
+import { Command, Option } from 'commander';
 
-class Sabik extends Command {
-  static description = 'This is source code metrics tool.';
-
-  static flags = {
-    version: flags.version({ char: 'v' }),
-    help: flags.help({ char: 'h' }),
-    outputFormat: flags.enum({
-      char: 't',
-      options: ['HTML', 'JSON'],
-      description: 'output report format. HTML or JSON. default: HTML',
-      default: 'HTML',
-    }),
-    outputReportPath: flags.string({
-      char: 'o',
-      description: `output report path.
-      For HTML, specify the directory, and for JSON, specify the file.`,
-      default: ({ flags: { outputFormat } }: { flags: { [key: string]: string } }) =>
-        outputFormat === 'HTML' ? `./sabik_report` : '',
-    }),
-    excludes: flags.string({
-      description: 'exclude patterns is separated by a comma. example: .test.ts$,.spec.ts$',
-      default: '$^',
-    }),
-    matches: flags.string({
-      description: 'match patterns. example: .ts$',
-      default: '.*',
-    }),
-  };
-
-  static args = [{ name: 'target' }];
-
-  async run() {
-    const { args, flags } = this.parse(Sabik);
-
-    const outputPath = flags.outputReportPath !== '' ? resolve(flags.outputReportPath) : null;
-    const analyzedTarget = resolve(args.target);
-    const excludes = (<string>flags.excludes).split(',').map((row) => new RegExp(row));
-    const matches = new RegExp(flags.matches);
+const program = new Command();
+program
+  .addOption(new Option('-t, --outputFormat <format>', 'output report format. HTML or JSON. default: HTML').choices(['HTML', 'JSON']).default('HTML'))
+  .requiredOption('--excludes <patterns...>', 'exclude patterns is separated by a comma. example: .test.ts$,.spec.ts$', '$^')
+  .requiredOption('--matches <pattern>', 'match patterns. example: .ts$', '.*')
+  .option('-o, --outputReportPath <path>', `output report path.
+For HTML, specify the directory, and for JSON, specify the file.`)
+  .arguments('[targetPath]')
+  .description('This is source code metrics tool.')
+  .action(async (targetPath?: string, options?) => {
+    const outputReportPath = options.outputReportPath ?? (options.outputFormat === 'HTML') ? './sabik_report' : null;
+    const outputPath = outputReportPath !== null ? resolve(outputReportPath) : null;
+    const analyzedTarget = resolve(targetPath ?? './');
+    const excludes = (<string>options.excludes).split(',').map((row) => new RegExp(row));
+    const matches = new RegExp(options.matches);
 
     if (!fs.existsSync(analyzedTarget)) {
-      return this.error(`${analyzedTarget}: No such file or directory.`);
+      throw new Error(`${analyzedTarget}: No such file or directory.`);
     }
 
     const rootPath = fs.statSync(analyzedTarget).isDirectory() ? analyzedTarget : dirname(analyzedTarget);
@@ -57,10 +34,13 @@ class Sabik extends Command {
     container.bind<RegExp>(Types.fileMatches).toConstantValue(matches);
     container.bind<RegExp[]>(Types.fileExcludes).toConstantValue(excludes);
 
-    const main = container.getNamed(Main, flags.outputFormat);
+    const main = container.getNamed(Main, options.outputFormat);
 
     main.exec(analyzedTarget);
-  }
-}
+  });
 
-export = Sabik;
+export = {
+  run: (version: string, argv: string[]) => program
+  .version(version, '-v, --version', 'show CLI version.')
+  .parseAsync(argv),
+};
