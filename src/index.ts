b952b0c3d2415a257fbb7c563b193b47c5c7f338
Tetsuro Yoshikawa <phper.0o0@gmail.com>
2022-05-16 23:57:51 +0900

    test: returns Promise, for wait to execute analizing.

--- src/index.ts
+++ src/index.ts
@@ -7,36 +7,37 @@ import { container } from './Sabik/DIContainer';
 import { Types } from './types/Types';
 import { Command, Option } from 'commander';
 
-const program = new Command();
-program
-  .addOption(
-    new Option('-t, --outputFormat <format>', 'output report format. default: HTML')
-      .choices(['HTML', 'JSON', 'CSV'])
-      .default('HTML')
-  )
+const generateCommand = () => {
+  const program = new Command();
+  program
+    .addOption(
+      new Option('-t, --outputFormat <format>', 'output report format. default: HTML')
+        .choices(['HTML', 'JSON', 'CSV'])
+        .default('HTML')
+    )
   .requiredOption(
     '--excludes <patterns...>',
     'exclude patterns is separated by a comma. example: .test.ts$,.spec.ts$',
     '$^'
   )
-  .requiredOption('--matches <pattern>', 'match patterns. example: .ts$', '.*')
-  .option(
-    '-o, --outputReportPath <path>',
-    `output report path.
-For HTML or CSV, specify the directory, and for JSON, specify the file.`
-  )
-  .arguments('[targetPath]')
-  .description('This is source code metrics tool.')
-  .action(async (targetPath?: string, options?) => {
+    .requiredOption('--matches <pattern>', 'match patterns. example: .ts$', '.*')
+    .option(
+      '-o, --outputReportPath <path>',
+      `output report path.
+Fo  r HTML or CSV, specify the directory, and for JSON, specify the file.`
+    )
+    .arguments('[targetPath]')
+    .description('This is source code metrics tool.')
+    .action(async (targetPath?: string, options?) => {
     const outputReportPath = options.outputReportPath ?? options.outputFormat === 'JSON' ? null : './sabik_report';
     const outputPath = outputReportPath !== null ? resolve(outputReportPath) : null;
     const analyzedTarget = resolve(targetPath ?? './');
     const excludes = (<string>options.excludes).split(',').map((row) => new RegExp(row));
     const matches = new RegExp(options.matches);
 
-    if (!fs.existsSync(analyzedTarget)) {
-      throw new Error(`${analyzedTarget}: No such file or directory.`);
-    }
+      if (!fs.existsSync(analyzedTarget)) {
+        throw new Error(`${analyzedTarget}: No such file or directory.`);
+      }
 
     const rootPath = fs.statSync(analyzedTarget).isDirectory() ? analyzedTarget : dirname(analyzedTarget);
 
@@ -44,13 +45,15 @@ For HTML or CSV, specify the directory, and for JSON, specify the file.`
     container.bind<string | null>(Types.outputPath).toConstantValue(outputPath);
     container.bind<RegExp>(Types.fileMatches).toConstantValue(matches);
     container.bind<RegExp[]>(Types.fileExcludes).toConstantValue(excludes);
+      const main = container.getNamed(Main, options.outputFormat);
 
-    const main = container.getNamed(Main, options.outputFormat);
+      await main.exec(analyzedTarget);
+    });
 
-    main.exec(analyzedTarget);
-  });
+  return program;
+};
 
 export = {
   run: (version: string, argv: string[]) =>
-    program.version(version, '-v, --version', 'show CLI version.').parseAsync(argv),
+    generateCommand().version(version, '-v, --version', 'show CLI version.').parseAsync(argv),
 };
